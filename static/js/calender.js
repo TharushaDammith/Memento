4b3bfeb8cafabac76c39f85a1a415b0956dda1c0
Tharusha <tharushadgunawardane@gmail.com>
2023-02-17 17:31:56 +0530

    feat add calender dark mode

--- static/js/calender.js
+++ static/js/calender.js
@@ -13,10 +13,16 @@ function OpenSchedule(elem) {
         item.remove();
     })
 
-    link_list.forEach((item) => {
-        event_list.insertAdjacentHTML('beforeend', `<li class="event-item"><a class="event-link" href="${item}">${item}</a></li>`);
-    });
-
+    if (dark_mode) {
+        link_list.forEach((item) => {
+            event_list.insertAdjacentHTML('beforeend', `<li class="event-item"><a class="event-link color-white" href="${item}">${item}</a></li>`);
+        });
+    }
+    else {
+        link_list.forEach((item) => {
+            event_list.insertAdjacentHTML('beforeend', `<li class="event-item"><a class="event-link" href="${item}">${item}</a></li>`);
+        });
+    }
     var overlay = document.querySelector('.overlay');
     overlay.classList.add('show-overlay');
 
@@ -30,3 +36,41 @@ function CloseSchedule(overlay) {
     var event_box = document.querySelector('.event-box');
     event_box.classList.remove('open-event-box');
 }
+
+
+// DARK/LIGHT MODE
+
+var light = document.querySelector('.light');
+var dark = document.querySelector('.dark');
+
+dark.addEventListener('click', () => {
+    var calender = document.querySelector('#Calender');
+    var all_boxes = document.querySelectorAll('.box');
+    var all_links = document.querySelectorAll('.event-link');
+
+    calender.classList.add('dark-bg');
+
+    all_boxes.forEach((item) => {
+        item.classList.add('dark-box');
+    });
+
+    all_links.forEach((item) => {
+        item.classList.add('color-white');
+    });
+})
+
+light.addEventListener('click', () => {
+    var calender = document.querySelector('#Calender');
+    var all_boxes = document.querySelectorAll('.box');
+    var all_links = document.querySelectorAll('.event-link');
+
+    calender.classList.remove('dark-bg');
+
+    all_boxes.forEach((item) => {
+        item.classList.remove('dark-box');
+    });
+
+    all_links.forEach((item) => {
+        item.classList.remove('color-white');
+    });
+})
